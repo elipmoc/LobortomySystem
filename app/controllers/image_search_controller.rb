0e3c420b4b36c427ca7a2f6da6d6366931ae0618
elipmoc <relipmoc101@gmail.com>
2018-04-29 17:57:33 +0900

    検索でセリフ検索もできるように

--- app/controllers/image_search_controller.rb
+++ app/controllers/image_search_controller.rb
@@ -4,7 +4,11 @@ class ImageSearchController < ApplicationController
   end
 
   def search
-    @images = Image.where('image_name LIKE ?', "%#{params[:text]}%").all
+    if(params[:colum][:name] == 'image_name')
+      @images = Image.where('image_name LIKE ?', "%#{params[:text]}%").all
+    elsif (params[:colum][:name] == 'serif_name')
+      @images = Image.where('serif_name LIKE ?', "%#{params[:text]}%").all
+    end
     render 'top'
   end
 end
