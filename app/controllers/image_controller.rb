e8919d44b4ca61656430c99160cbfa00b9872969
elipmoc <relipmoc101@gmail.com>
2018-04-29 17:58:04 +0900

    セリフを編集できるように

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,4 +2,22 @@ class ImageController < ApplicationController
   def index
     @images = Image.all
   end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+  def edit
+    @image = Image.find(params[:id])
+  end
+
+  def update
+    @image = Image.find(params[:id])
+    @image.update(images_params)
+    render 'show'
+  end
+
+  def images_params
+    params.require(:image).permit(:serif_name)
+  end
 end
